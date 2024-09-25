9b2013a591aab0fd5ad01cf8ab2b918a10860a84
Anzhelika-g <anzh.ghazaryan@gmail.com>
2024-09-25 21:57:32 +0400

    Add Patient

---
+++ src/main/java/org/example/hospital/dto/PatientDTO.java
@@ -0,0 +1,11 @@
+package org.example.hospital.dto;
+
+import lombok.Data;
+
+@Data
+public class PatientDTO {
+    private Long patientId;
+    private String name;
+    private String idCard;
+    private String phoneNumber;
+}
